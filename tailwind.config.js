07aab89bf89cb24fbbcdd75f0f328874b78f3982
r3m00n <merlin.foerster@live.de>
2022-02-16 01:13:20 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["HelveticaNeueLT55Roman", "Helvetica", "Arial", "sans-serif"],
+        now: ["HelveticaNowDisplay", "Helvetica", "Arial", "sans-serif"],
+        tiposka: ["Tiposka", "Helvetica", "Arial", "sans-serif"],
+      },
+      screens: {
+        s: "500px",
+      },
+    },
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      turquoise: "#73ffdf",
+      green: "#1a5e4f",
+    },
+  },
+  plugins: [],
+};
